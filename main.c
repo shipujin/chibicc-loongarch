29489958e40e91213ff6061ed38fa2d987c9e019
Shi Pujin <shipujin.t@gmail.com>
2022-08-08 02:23:43 +0000

    Add + and - operators

--- main.c
+++ main.c
@@ -7,9 +7,29 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1];
+
   printf("  .globl main\n");
   printf("main:\n");
-  printf("  li.d $a0, %d\n", atoi(argv[1]));
+  printf("  li.d $a0, %ld\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  addi.d $a0, $a0, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  addi.d $a0, $a0, -%ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "unexpected character: '%c'\n", *p);
+    return 1;
+  }
+
   printf("  jr $ra\n");
 
   printf(".LFE0:\n");
