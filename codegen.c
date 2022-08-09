84aa693b46a440996e6d517bb2b3e171b21369e9
Shi Pujin <shipujin.t@gmail.com>
2022-08-09 03:19:25 +0000

    Support single-letter local variables

--- codegen.c
+++ codegen.c
@@ -12,6 +12,19 @@ static void pop(char *arg) {
   printf("  ld.d $%s, $sp, -%d\n", arg, depth * 8);
 }
 
+// Compute the absolute address of a given node.
+// It's an error if a given node does not reside in memory.
+static void gen_addr(Node *node) {
+  if (node->kind == ND_VAR) {
+    int offset = (node->name - 'a' + 1) * 8;
+    printf("  addi.d $a0, $fp,%d\n", -offset);
+    return;
+  }
+
+  error("not an lvalue");
+}
+
+// Generate code for a given node.
 static void gen_expr(Node *node) {
   switch (node->kind) {
   case ND_NUM:
@@ -21,6 +34,18 @@ static void gen_expr(Node *node) {
     gen_expr(node->lhs);
     printf("  sub.d $a0, $r0, $a0\n");
     return;
+  case ND_VAR:
+    gen_addr(node);
+    printf("  ld.d $a0, $a0, 0\n");
+    return;
+  case ND_ASSIGN:
+    gen_addr(node->lhs);
+    push();
+    gen_expr(node->rhs);
+    pop("a1");
+    printf("  st.d $a0, $a1, 0\n");
+    return;
+
   }
 
   gen_expr(node->rhs);
@@ -78,11 +103,23 @@ void codegen(Node *node) {
   printf("  .globl main\n");
   printf("main:\n");
 
+  // Prologue
+  printf("  addi.d $sp, $sp, -224\n");
+  printf("  st.d $fp, $sp, 216\n");
+  printf("  st.d $ra, $sp, 208\n");
+  printf("  add.d $fp, $r0, $sp\n");
+
+  printf("  addi.d $sp, $sp, -208\n");
+
   for (Node *n = node; n; n = n->next) {
     gen_stmt(n);
     assert(depth == 0);
   }
 
+  printf("  add.d $sp, $r0, $fp\n");
+  printf("  ld.d $fp, $sp, 216\n");
+  printf("  ld.d $ra, $sp, 208\n");
+  printf("  addi.d $sp, $sp, 224\n");
   printf("  jr $ra\n");
 
   printf(".LFE0:\n");
