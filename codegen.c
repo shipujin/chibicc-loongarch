7986e91641dcca782b6f1fc43273a6507bec1968
Shi Pujin <shipujin.t@gmail.com>
2022-08-17 08:45:30 +0000

    Add switch-case

--- codegen.c
+++ codegen.c
@@ -338,6 +338,25 @@ static void gen_stmt(Node *node) {
     println("%s:", node->brk_label);
     return;
   }
+  case ND_SWITCH:
+    gen_expr(node->cond);
+
+    for (Node *n = node->case_next; n; n = n->case_next) {
+      println("  li.d $a4, %ld", n->val);
+      println("  beq $a0, $a4, %s", n->label);
+    }
+
+    if (node->default_case)
+      println("  b %s", node->default_case->label);
+
+    println("  b %s", node->brk_label);
+    gen_stmt(node->then);
+    println("%s:", node->brk_label);
+    return;
+  case ND_CASE:
+    println("%s:", node->label);
+    gen_stmt(node->lhs);
+    return;
   case ND_BLOCK:
     for (Node *n = node->body; n; n = n->next)
       gen_stmt(n);
